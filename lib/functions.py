775c53b442ae0f3e451012dbc60150ab4fdddf2d
ajin3830 <ajin3830@gmail.com>
2023-02-23 11:32:04 -0500

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
+# greet_programmer()
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
+# greet('name')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+# greet('name')
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
+# add(1, 2)
 
 def halve(number):
-    pass
+    halve_num = number / 2
+    return halve_num
+# halve(2)
